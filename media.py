155b038e90b5fa8e90345b90f1e20884b2b248b8
mhhoban <mhhoban@gmail.com>
2016-11-05 11:27:02 -0700

    added comments and docstring for media.py

--- media.py
+++ media.py
@@ -1,14 +1,23 @@
 
 class Movie:
+    """
+    The movie object reads in the attributes of a movie to create a data
+    structure for accessing these attributes of the movie.
+
+    """
 
     def __init__(self, title, summary, poster, trailer_youtube_url):
-        # title of the movie
+        """
+        Initializes the object with the following attributes:
+
+        :param title: Title of the movie
+        :param summary: a brief summary of the movie
+        :param poster: a link to an image of the movie's poster
+        :param trailer_youtube_url: a link to the movie's trail on youtube
+        """
         self.title = title
-        # a summary of the movie
         self.summary = summary
-        # a URL for a poster for the movie
         self.poster_image_url = poster
-        # a URL for a trailer for the movie on youtube.
         self.trailer_youtube_url = trailer_youtube_url
 
 
