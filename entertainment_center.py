af1a8f01fe16930a561a49120c5d91624cf43c93
mhhoban <mhhoban@gmail.com>
2016-11-05 10:09:29 -0700

    Made things more PEP8 friendly

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,64 @@
 import media
 import fresh_tomatoes
 
-aliens = media.Movie('Aliens', 'The face suckers return',
-                     'https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg',
+# instantiate Movie object for "Aliens"
+aliens = media.Movie('Aliens',
+                     'The face suckers return',
+                     'https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_'
+                     'poster.jpg',
                      'https://www.youtube.com/watch?v=AW-7_HE98PY'
                      )
 
-conan_the_barbarian = media.Movie('Conan', 'Conan Slays.',
-                                  'https://upload.wikimedia.org/wikipedia/en/c/cd/Conan_the_'
-                                  'Barbarian_1982_film_poster.jpg',
+# instantiate Movie object for "Conan The Barbarian"
+conan_the_barbarian = media.Movie('Conan',
+                                  'Conan Slays.',
+                                  'https://upload.wikimedia.org/wikipedia/en/'
+                                  'c/cd/Conan_the_Barbarian_1982_film_poster'
+                                  '.jpg',
                                   'https://www.youtube.com/watch?v=xwdYd_RdLCQ'
                                   )
 
-man_from_earth = media.Movie('The Man From Earth', 'Cave man Professor lays down knowledge',
-                             'https://upload.wikimedia.org/wikipedia/en/3/3b/The_Man_from_Earth.png',
+# instantiate Movie object for "The Man From Earth"
+man_from_earth = media.Movie('The Man From Earth',
+                             'Cave man Professor lays'
+                             'down knowledge',
+                             'https://upload.wikimedia.org/wikipedia/en/3/3b/'
+                             'The_Man_from_Earth.png',
                              'https://www.youtube.com/watch?v=lVMhEAI3pvg'
                              )
 
-interview_with_a_vampire = media.Movie('Interview With The Vampire', 'The Vampire Tells All',
-                            'https://upload.wikimedia.org/wikipedia/en/f/fe/InterviewwithaVampireMoviePoste.JPG',
-                            'https://www.youtube.com/watch?v=bDH7P0qvSMU'
-                            )
+# instantiate Movie object for "Interview With The Vampire"
+interview_with_a_vampire = media.Movie('Interview With The Vampire',
+                                       'The Vampire Tells All',
+                                       'https://upload.wikimedia.org/wikipedia'
+                                       '/en/f/fe/InterviewwithaVampireMoviePos'
+                                       'te.JPG',
+                                       'https://www.youtube.com/watch?v=bDH7P'
+                                       '0qvSMU'
+                                       )
 
-commando = media.Movie('Commando', 'Arnold plays arnold.',
-            'https://upload.wikimedia.org/wikipedia/en/d/d9/Commandoposter.jpg',
-            'https://www.youtube.com/watch?v=mh-QUh69MCg'
-            )
+# instantiate Movie object for "Commando"
+commando = media.Movie('Commando',
+                       'Arnold plays Arnold.',
+                       'https://upload.wikimedia.org/wikipedia/en/d/d9/Command'
+                       'oposter.jpg',
+                       'https://www.youtube.com/watch?v=mh-QUh69MCg'
+                       )
 
-army_of_darkness = media.Movie('Army of Darkness', 'Ash saves, then dooms, then saves the primitive screw heads.',
-                    'https://upload.wikimedia.org/wikipedia/en/4/46/Army_of_Darkness_poster.jpg',
-                    'https://www.youtube.com/watch?v=CZ-wU5RXw2o'
-                    )
+# instantiate Movie object for "Army of Darkness"
+army_of_darkness = media.Movie('Army of Darkness',
+                               'Ash saves, then dooms, then saves the primitiv'
+                               'e screw heads.',
+                               'https://upload.wikimedia.org/wikipedia/en/4/46'
+                               '/Army_of_Darkness_poster.jpg',
+                               'https://www.youtube.com/watch?v=CZ-wU5RXw2o'
+                               )
 
-movies = [aliens, conan_the_barbarian, man_from_earth, interview_with_a_vampire, commando, army_of_darkness]
+# create list of the movie objects created above
+movies = [aliens, conan_the_barbarian, man_from_earth,
+          interview_with_a_vampire, commando, army_of_darkness]
 
-
+# pass the list of movie object into Fresh_Tomatoes and generate the website
+# using the movie objects created above.
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -2,9 +2,13 @@
 class Movie:
 
     def __init__(self, title, summary, poster, trailer_youtube_url):
+        # title of the movie
         self.title = title
+        # a summary of the movie
         self.summary = summary
+        # a URL for a poster for the movie
         self.poster_image_url = poster
+        # a URL for a trailer for the movie on youtube.
         self.trailer_youtube_url = trailer_youtube_url
 
 
